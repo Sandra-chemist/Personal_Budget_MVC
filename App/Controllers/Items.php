d18dd793500e09277f071c2fb8df3c645f0cca5d
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-11 16:07:04 +0200

    added a method to the core controller for requiring logib

--- App/Controllers/Items.php
+++ App/Controllers/Items.php
@@ -5,16 +5,22 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Auth;
 
+/**
+ * Items controller (example)
+ *
+ * PHP version 7.0
+ */
 class Items extends \Core\Controller
 {
 
+    /**
+     * Items index
+     *
+     * @return void
+     */
     public function indexAction()
     {
-        if (!Auth::isLoggedIn()) {
-
-            Auth::rememberRequestedPage();
-            $this->redirect('/login');
-        }
+        $this->requireLogin();
 
         View::renderTemplate('Items/index.html');
     }
