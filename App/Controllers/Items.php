4cc3e593346d73b03718906af666d359b21af821
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-11 15:44:29 +0200

    added new class: Items

---
+++ App/Controllers/Items.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+class Items extends \Core\Controller
+{
+
+    public function indexAction()
+    {
+        if (!Auth::isLoggedIn()) {
+            $this->redirect('/login');
+        }
+
+        View::renderTemplate('Items/index.html');
+    }
+}
