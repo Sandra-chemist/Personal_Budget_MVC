8f22bcbb2fa7fdfd0d32b96762e9c2a2ca194d71
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-11 17:54:25 +0200

    added method getMessages in Flash class

--- App/Flash.php
+++ App/Flash.php
@@ -10,4 +10,14 @@ class Flash{
 
         $_SESSION['flash_notification'][] = $message;
     }
+
+    public static function getMessages(){
+        if (isset($_SESSION['flash_notification'])) {
+            $messages = $_SESSION['flash_notification'];
+            
+            unset($_SESSION['flash_notification']);
+
+            return $messages;
+        }
+    }
 }
