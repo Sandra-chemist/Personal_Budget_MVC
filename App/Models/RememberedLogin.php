8aef0b7d64347d16482880058c62a313de74ea31
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-12 11:10:21 +0200

    added hasExpired function

--- App/Models/RememberedLogin.php
+++ App/Models/RememberedLogin.php
@@ -2,7 +2,24 @@
 
 namespace App\Models;
 
-class RememberedLogin extends \Core\Model{
+use PDO;
+use \App\Token;
+
+/**
+ * Remembered login model
+ *
+ * PHP version 7.0
+ */
+class RememberedLogin extends \Core\Model
+{
+
+    /**
+     * Find a remembered login model by the token
+     *
+     * @param string $token The remembered login token
+     *
+     * @return mixed Remembered login object if found, false otherwise
+     */
     public static function findByToken($token){
         $token = new Token($token);
         $token_hash = $token->getHash();
@@ -22,6 +39,10 @@ class RememberedLogin extends \Core\Model{
     }
 
     public function getUser(){
-        return User::findByID($this->id);
+        return User::findByID($this->user_id);
+    }
+
+    public function hasExpired(){
+        return strtotime($this->expires_at) < time();
     }
 }
