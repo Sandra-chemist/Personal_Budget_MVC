17ba387f8d7f955ab985a3626297873e6fb1e4ab
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-12 10:03:58 +0200

    added: remember the login in the database

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -25,7 +25,7 @@ class Login extends \Core\Controller{
 
       if ($user) {
 
-         Auth::login($user);
+         Auth::login($user, $remember_me);
 
          Flash::addMessage('Zalogowano poprawnie!');
 
