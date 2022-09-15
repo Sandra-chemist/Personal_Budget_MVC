402b309ce89bb71cc32e7384055cc8b3ba8ced07
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-15 14:28:55 +0200

    created password reset form

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -21,6 +21,10 @@ class Password extends \Core\Controller{
 
         $user = User::findByPasswordReset($token);
 
-        var_dump($user);
+        if ($user){
+            View::renderTemplate('Password/reset.html');
+        } else{
+            echo "Token resetowania hasła jest nieprawidłowy";
+        }
     }
 }
