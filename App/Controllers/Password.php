d5b3ef30b5178bcb9882a5cb0311442d2d5cffe6
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-15 15:08:06 +0200

    added expired token view

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -19,12 +19,42 @@ class Password extends \Core\Controller{
     public function resetAction(){
         $token = $this->route_params['token'];
 
+        $user = $this->getUserOrExit($token);
+
+            View::renderTemplate('Password/reset.html', [
+                      'token' => $token 
+            ]);
+        
+    }
+    public function resetPasswordAction()
+    {
+        $token = $_POST['token'];
+
+        $user = $this->getUserOrExit($token);
+
+        echo "Zresetuj hasło użytkownika tutaj";
+    }
+
+    /**
+     * Find the user model associated with the password reset token, or end the request with a message
+     *
+     * @param string $token Password reset token sent to user
+     *
+     * @return mixed User object if found and the token hasn't expired, null otherwise
+     */
+    protected function getUserOrExit($token)
+    {
         $user = User::findByPasswordReset($token);
 
-        if ($user){
-            View::renderTemplate('Password/reset.html');
-        } else{
-            echo "Token resetowania hasła jest nieprawidłowy";
+        if ($user) {
+
+            return $user;
+
+        } else {
+
+            View::renderTemplate('Password/token_expired.html');
+            exit;
+
         }
     }
 }
