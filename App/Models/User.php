f4cb7f26d992ae24a0da8c80206ab1b36039d0bc
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-15 20:00:25 +0200

    added: generate a unique activation token upon signup

--- App/Models/User.php
+++ App/Models/User.php
@@ -30,8 +30,11 @@ class User extends \Core\Model
 
             $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
 
-            $sql = 'INSERT INTO users (username, email, password)
-                    VALUES (:username, :email, :password)';
+            $token = new Token();
+            $hashed_token = $token->getHash();
+
+            $sql = 'INSERT INTO users (username, email, password, activation_hash)
+                    VALUES (:username, :email, :password, :activation_hash)';
             
             $db = static::getDB();
             $stmt = $db->prepare($sql);
@@ -39,6 +42,7 @@ class User extends \Core\Model
             $stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
             $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
             $stmt->bindValue(':password', $password_hash, PDO::PARAM_STR);
+            $stmt->bindValue(':activation_hash', $hashed_token, PDO::PARAM_STR);
 
             return $stmt->execute();
         }
