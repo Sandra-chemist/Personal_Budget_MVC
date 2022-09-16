5af2b7495b194f66ef4e6cbfe1f39a5d4348b55a
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-16 13:59:11 +0200

    added show.html to Profile folder

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Controllers;
+
 use \Core\View;
+use \App\Auth;
 
 class Profile extends Authenticated{
     public function showAction(){
-        View::renderTemplate('Profile/show.html');
+        View::renderTemplate('Profile/show.html', [
+            'user' => Auth::getUser()
+        ]);
     }
 }
