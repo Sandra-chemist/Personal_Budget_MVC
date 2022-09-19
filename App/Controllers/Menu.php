5a2e9d648197b114f6016ae7605e17b2c96cf1be
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-19 11:33:31 +0200

    added buttons to the menu page

--- App/Controllers/Menu.php
+++ App/Controllers/Menu.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use \Core\View;
 
 class Menu extends Authenticated{
-    public function mainAction(){
-        View::renderTemplate('Menu/show_menu.html');
+    public function indexAction(){
+        View::renderTemplate('Menu/index.html');
     }
 }
