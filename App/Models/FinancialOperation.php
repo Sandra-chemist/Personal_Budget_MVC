48cee9136d4ca555775661be6dca86b9e2e3fa46
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-19 19:53:36 +0200

    added comment validation

--- App/Models/FinancialOperation.php
+++ App/Models/FinancialOperation.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use PDO;
 
 class FinancialOperation extends \Core\Model{
+    public $errors = [];
+
     public function __construct($financialOperation = [])
     {
         foreach ($financialOperation as $key => $value) {
@@ -12,16 +14,16 @@ class FinancialOperation extends \Core\Model{
         };
     }
     public function saveIncome(){
+        $this->validate();
 
         $income_category_id = 1;
+
+        if (empty($this->errors)){
         $sql = 'INSERT INTO incomes (user_id, income_category_assigned_to_user_id, amount, date_of_income, income_comment)
                     VALUES (:user_id, :income_category_assigned_to_user_id, :amount, :date_of_income, :income_comment)';
 
         $db = static::getDB();
         $stmt = $db->prepare($sql);
-
-
-        $amount = '';
 
         $stmt->bindValue(':user_id', $_SESSION['id'], PDO::PARAM_INT);
         $stmt->bindValue(':income_category_assigned_to_user_id', $income_category_id, PDO::PARAM_STR);
@@ -30,5 +32,13 @@ class FinancialOperation extends \Core\Model{
         $stmt->bindValue(':income_comment', $this->comment, PDO::PARAM_STR);
 
         return $stmt->execute();
+        }
+        return false;
+    }
+
+    public function validate(){
+        if (strlen($this->comment > 100)) {
+            $this->errors[] = 'Komentarz nie może mieć więcej niż 100 znaków';
+        }
     }
 }
