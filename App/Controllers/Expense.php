c66fe627729a42a5d8e7e8d3856a6cc5438780a1
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-21 13:50:39 +0200

    added display of message after adding income or expense

--- App/Controllers/Expense.php
+++ App/Controllers/Expense.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Models\FinancialOperation;
+use \App\Flash;
 
 class Expense extends Authenticated{
     public function indexAction(){
@@ -14,6 +15,7 @@ class Expense extends Authenticated{
         $financialOperation = new FinancialOperation($_POST);
 
         if ($financialOperation->saveExpense()) {
+            Flash::addMessage('Wydatek został poprawnie dodany!');
             $this->redirect('/Menu/index');
         } else {
             View::renderTemplate('Expense/index.html', [
--- App/Controllers/Income.php
+++ App/Controllers/Income.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Models\FinancialOperation;
+use \App\Flash;
 
 class Income extends Authenticated{
     public function indexAction(){
@@ -14,6 +15,7 @@ class Income extends Authenticated{
         $financialOperation = new FinancialOperation($_POST);
 
         if ($financialOperation->saveIncome()) {
+            Flash::addMessage('Przychód został poprawnie dodany');
             $this->redirect('/Menu/index');
         } else {
             View::renderTemplate('Income/index.html', [
