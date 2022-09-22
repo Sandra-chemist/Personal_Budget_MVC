adf5d99b719ea4f0c883e54c3f9b5602ffca03c7
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-22 11:48:41 +0200

    added functions: getCurrentMonthStartDate and getCurrentMonthEndDate

--- App/Date.php
+++ App/Date.php
@@ -6,4 +6,12 @@ class Date{
     public static function getCurrentDate(){
         return date("Y-m-d");
     }
+
+    public static function getCurrentMonthStartDate(){
+        return date("Y-m-01");
+    }
+
+    public static function getCurrentMonthEndDate(){
+        return date("Y-m-t");
+    }
 }
---
+++ App/Models/Balance.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+use \App\Date;
+
+class Balance extends \Core\Model{
+
+}
