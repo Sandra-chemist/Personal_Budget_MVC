5403bcae1eb8ce162b824506ffaf4a9904f5dd34
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-09-22 08:16:12 +0200

    improved appearance

--- App/Mail.php
+++ App/Mail.php
@@ -50,7 +50,6 @@ class Mail{
 
 
             $mail->send();
-            echo 'Wiadomość została wysłana';
         } catch (Exception $e) {
             echo "Wiadomość nie może być wysłana. Mailer Error: {$mail->ErrorInfo}";
         }
--- App/Models/User.php
+++ App/Models/User.php
@@ -181,7 +181,7 @@ class User extends \Core\Model{
         $text = View::getTemplate('Password/reset_email.txt', ['url' => $url]);
         $html = View::getTemplate('Password/reset_email.html', ['url' => $url]);
 
-        Mail::send($this->email, 'Resetowanie_hasła', $text, $html);
+        Mail::send($this->email, 'Resetowanie hasła', $text, $html);
      }
 
      public static function findByPasswordReset($token){
