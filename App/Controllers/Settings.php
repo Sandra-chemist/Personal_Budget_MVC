2386f51541d47e6ecb025d39df8682e838fb43cb
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-10-11 12:33:20 +0200

    added pages wiht categories incomes and expenses and payment methods

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -8,4 +8,16 @@ class Settings extends Authenticated{
     public function indexAction(){
         View::renderTemplate('Settings/index.html');
     }
+
+    public function incomeCategoriesAction(){
+        View::renderTemplate('Settings/incomeCategories.html');
+    }
+
+    public function expenseCategoriesAction(){
+        View::renderTemplate('Settings/expenseCategories.html');
+    }
+
+    public function paymentMethodsAction(){
+        View::renderTemplate('Settings/paymentMethods.html');
+    }
 }
