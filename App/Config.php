cacba0ea80355bf984fb543b855ff58eceb4da5c
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-10-11 11:28:02 +0200

    improved appearance application

--- App/Config.php
+++ App/Config.php
@@ -7,20 +7,20 @@ class Config
 
     const DB_HOST = 'localhost';
 
-    const DB_NAME = '';
+    const DB_NAME = 'mvcpersonalbudget';
 
-    const DB_USER = '';
+    const DB_USER = 'root';
 
     const DB_PASSWORD = '';
 
     const SHOW_ERRORS = true;
 
-    const SECRET_KEY ='';
+    const SECRET_KEY = 'Alf4WgE0AB8YlR3fWXK33Lm5Bz8awQQh';
     
     const SMTP = 'smtp.gmail.com';  
     
-    const MAIL_ADDRESS = '';
+    const MAIL_ADDRESS = 'sandraskibiszewska@gmail.com';
 
-    const PASSWORD = ''; 
+    const PASSWORD = 'kftfdmhqfpvbwzxr';
     
 }
