7e8a7ebbbac0904cd92bae525f78a6feb37c8416
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-10-11 12:15:46 +0200

    created settings class

---
+++ App/Controllers/Settings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+class Settings extends Authenticated{
+    public function indexAction(){
+        View::renderTemplate('Settings/index.html');
+    }
+}
