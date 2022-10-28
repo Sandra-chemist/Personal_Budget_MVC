837387895938f4997ce45c4c187c06dde5333f10
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-10-28 13:51:06 +0200

    added display of the limit and expenses to the applicaton

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
 const categoryField = document.querySelector('select[name="category"]');
 const monthlyLimit = document.querySelector('.monthlyLimit');
+const sumExpenses = document.querySelector('.sumExpenses');
+const info = document.querySelector('.info');
+const box = document.querySelector('.box');
 
 const date = new Date();
 const currentMonth = date.getMonth() + 1;
@@ -21,16 +24,12 @@ const nextMonthStart = year + '-' + nextMonth + '-' + '01';
 console.log(nextMonthStart);
 
 let sum = 0;
+let limit;
 
 categoryField.addEventListener('change', function () {
 	if (categoryField.value) {
 		const category = categoryField.options[categoryField.selectedIndex].value;
 		console.log(category);
-
-		/*fetch('/expense/limit')
-			.then(res => res.json())
-			.then(data => console.log(data))
-			.catch(err => console.log(err));*/
 
 		fetch('/expense/limit')
 			.then(res => res.json())
@@ -38,34 +37,58 @@ categoryField.addEventListener('change', function () {
 				for (let i = 0; i < data.length; i++) {
 					if (data[i].name === category && data[i].monthly_limit !== 0) {
 						monthlyLimit.textContent = `Miesięczny limit dla tej kategorii wynosi: ${data[i].monthly_limit} zł`;
-						console.log(`${data[i].monthly_limit}`);
+						limit = parseFloat(data[i].monthly_limit);
+						console.log(limit);
+
+						fetch('/expense/sumMonthlyExpenses')
+							.then(res => res.json())
+							.then(expenses => {
+								for (let j = 0; j < expenses.length; j++) {
+									if (
+										expenses[j].name === category &&
+										expenses[j].date_of_expense >= currentMonthStart &&
+										expenses[j].date_of_expense < nextMonthStart
+									) {
+										console.log(expenses);
+										console.log(
+											`${expenses[j].date_of_expense} ${expenses[j].amount}`
+										);
+
+										sum = sum + parseFloat(expenses[j].amount);
+										sumExpenses.textContent = `Dotychczasowa suma wydatków: ${sum} zł`;
+									}
+
+									console.log(sum);
+									if (sum <= limit) {
+										console.log(`suma to ${sum}`);
+										info.textContent =
+											'Jeszcze nie przekroczyłeś limitu wydatków!';
+										info.style.backgroundColor = '#006400';
+										info.style.color = '#ffffff';
+									} else {
+										info.textContent =
+											'Przekroczyłeś limit miesięcznych wydatków dla tej kategorii!';
+										info.style.backgroundColor = '#a4133c';
+										info.style.color = '#ffffff';
+									}
+								}
+							})
+							.catch(err => console.log(err));
 					} else if (data[i].name === category && data[i].monthly_limit === 0) {
 						monthlyLimit.textContent = '';
+						sumExpenses.textContent = '';
+						info.textContent = '';
 					}
-				}
-			})
-			.catch(err => console.log(err));
-
-		fetch('/expense/sumMonthlyExpenses')
-			.then(res => res.json())
-			.then(expenses => {
-				for (let j = 0; j < expenses.length; j++) {
-					if (
-						expenses[i].name === category &&
-						expenses[j].date_of_expense >= currentMonthStart &&
-						expenses[j].date_of_expense < nextMonthStart
-					) {
-						console.log(expenses);
-						console.log(`${expenses[j].date_of_expense} ${expenses[j].amount}`);
-						sum = sum + parseFloat(expenses[j].amount);
-					}
-					console.log(sum);
 				}
 			})
 			.catch(err => console.log(err));
 
 		monthlyLimit.classList.remove('hidden');
+		sumExpenses.classList.remove('hidden');
+		info.classList.remove('hidden');
 	} else {
 		monthlyLimit.classList.add('hidden');
+		sumExpenses.classList.add('hidden');
+		info.classList.add('hidden');
 	}
 });
